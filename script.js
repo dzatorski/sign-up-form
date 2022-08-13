07074d1768d393d6fa53288c47ba45262fd0fb7f
dzatorski <damian2222221@gmail.com>
2022-08-13 13:52:04 +0200

    Added password verifiaction

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const password = document.querySelector(`#password`);
-const confirmPassword = document.querySelector(`#confirm_password`);
-password.textContent = `XDDDD`;
+let password = document.getElementById(`password`).value;
+let confirmPassword = document.getElementById(`confirm_password`).value;
+const signButton = document.querySelector(`.sign-up-button`);
+const passwordContainer = document.querySelector(`#password`);
+const passwordConfContainer = document.querySelector(`#confirm_password`);
+
+signButton.addEventListener(`click`, () => {
+  confirmPassword = document.getElementById(`confirm_password`).value;
+  password = document.getElementById(`password`).value;
+
+  confirmPassword === password
+    ? console.log(`Matching passwords`)
+    : ((passwordContainer.style.border = `1px solid red`),
+      (passwordConfContainer.style.border = `1px solid red`));
+});
